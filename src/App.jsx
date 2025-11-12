12e03763ef2a2fd06784324f755da5524013cb2c
girishsai22 <girishsain22@gmail.com>
2025-11-12 13:25:34 +0530

    Update frontend for templates_to_pdf feature

--- src/App.jsx
+++ src/App.jsx
@@ -44,6 +44,7 @@ import ConsultantTeamPage from "./Components/HomeNav/ConsultantTeamPage";
 import VaptQuestions from "./Components/AdminView/ProjectTabs/VaptQuestions";
 import VaptForm from "./Components/AdminView/ProjectTabs/VaptForm";
 import EditorView from "./Components/EditorView/EditorView";
+import PolicyEditor from "./Components/PolicyEditor/PolicyEditor";
 
 const router = createBrowserRouter([
   {
@@ -213,6 +214,10 @@ const router = createBrowserRouter([
       {
         path: "editor",
         element: <EditorView />,
+      },
+      {
+        path: "policy-editor/:policyId?",
+        element: <PolicyEditor />,
       },
     ],
   },
---
+++ src/Components/PolicyEditor/index.js
@@ -0,0 +1,5 @@
+export { default as PolicyEditor } from "./PolicyEditor";
+export { default as PolicyTemplateList } from "./PolicyTemplateList";
+export { default as RichTextEditor } from "./RichTextEditor";
+export { default as AIPanel } from "./AIPanel";
+
